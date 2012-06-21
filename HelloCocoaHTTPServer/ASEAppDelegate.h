496eb4431851c57f506df510d061dc1e8f08096e
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-06-21 09:41:21 -0600

    Initial import

---
+++ HelloCocoaHTTPServer/ASEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASEAppDelegate.h
+//  HelloCocoaHTTPServer
+//
+//  Created by O'Keeffe Arlo Louis on 2012-06-21.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ASEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
